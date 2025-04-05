3f3c765847f5229613f16201f3f55545ccfe16d4
shlomtzionm <shlomtzosh@gmail.com>
2025-04-05 20:03:43 +0300

    add style

--- src/components/logo/Logo.tsx
+++ src/components/logo/Logo.tsx
@@ -1,21 +1,44 @@
-import { JSX } from "react";
+import { JSX, useEffect } from "react";
 import "./logo.css";
+import gsap from "gsap";
+import { ScrollTrigger } from "gsap/ScrollTrigger";
+
+// Register ScrollTrigger plugin
+gsap.registerPlugin(ScrollTrigger);
 
 function Logo(): JSX.Element {
+  useEffect(() => {
+    const tl = gsap.timeline({
+      scrollTrigger: {
+        trigger: ".textShadow",
+        start: "top 50%",
+        end: "bottom 20%",
+        scrub: true
+      }
+    });
+
+
+    tl.to(".textShadow", {
+      textShadow: "3px 3px 0 #ffdac0",
+      duration: 0.5,
+    })
+      .to(".textShadow", {
+        textShadow: "3px 3px 0 #ffdac0,6px 6px 0 #ffa666",
+        duration: 0.5,
+      })
+      .to(".textShadow", {
+        textShadow: "3px 3px 0 #ffdac0,6px 6px 0 #ffa666,9px 9px 0 #e37e32",
+        duration: 0.5,
+      })
+      .to(".textShadow", {
+        textShadow: "3px 3px 0 #ffdac0,6px 6px 0 #ffa666,9px 9px 0 #e37e32,12px 12px 0 #b56900",
+        duration: 0.5,
+      });
+  }, []);
 
   return (
     <div className="logo">
-      <h1>
-        <span>s</span>
-        <span>h</span>
-        <span>l</span>
-        <span>o</span>
-        <span>m</span>
-        <span>t</span>
-        <span>i</span>
-        <span>o</span>
-        <span>n</span>
-      </h1>
+      <h1 className="textShadow">shlomtzion</h1>
     </div>
   );
 }
--- src/components/open/Open.tsx
+++ src/components/open/Open.tsx
@@ -1,79 +1,56 @@
-import { JSX, useEffect, useRef } from "react";
-import "../open/open.css";
+import { JSX, useEffect, useState } from "react";
+import "./open.css";
 import gsap from "gsap";
 import { ScrollTrigger } from "gsap/ScrollTrigger";
+import SplitType from "split-type";
 
-// Register ScrollTrigger
 gsap.registerPlugin(ScrollTrigger);
 
 function Open(): JSX.Element {
-  const elements = useRef<HTMLSpanElement[]>([]);
+  const skills = [
+   "react", "angular", "mySQL", "mongoDB", "typeScript", "restAPI", 
+    "Socket.io", "OAuth", "Docker", "AWS", "Mocha", "chai", "node.js", "angular", 
+    "mySQL", "mongoDB", "typeScript", "restAPI", "Socket.io", "OAuth", "Docker", 
+    "AWS", "Mocha", "chai", "node.js","mui"
+  ];
+
+ 
 
   useEffect(() => {
-    console.log(elements.current);
 
-    const tl = gsap.timeline({
-      defaults: { duration: 0.2, ease: "back.inOut" },
-    });
+    const splitText = new SplitType(".skillContainer", { types: "chars" });
 
-    tl.to(elements.current[0], { opacity: 1, scale: 0.5, x: 190, y: -200 })
-      .to(elements.current[1], { opacity: 1, scale: 1, x: 90, y: -40 })
-      .to(elements.current[2], { opacity: 1, scale: 1.5, x: -300, y: 10 })
-      .to(elements.current[3], { opacity: 1, scale: 1.3, x: -290, y: -340 })
-      .to(elements.current[4], { opacity: 1, scale: 1.2, x: 200, y: -300 })
-      .to(elements.current[5], { opacity: 1, scale: 1.7, x: -130, y: 170 })
-      .to(elements.current[6], { opacity: 1, scale: 0.6, x: -50, y: -170 })
-      .to(elements.current[7], { opacity: 1, scale: 0.8, x: -210, y: -40 })
-      .to(elements.current[8], { opacity: 1, scale: 0.4, x: -360, y: 100 })
-      .to(elements.current[9], { opacity: 1, scale: 0.5, x: -300, y: -270 })
-      .to(elements.current[10], { opacity: 1, scale: 1.1, x: 200, y: 200 })
-      .to(elements.current[11], { opacity: 1, scale: 0.6, x: -190, y: -240 })
-      .to(elements.current[12], { opacity: 1, scale: 1, x: -270, y: -185 })
-      .to(elements.current[13], { opacity: 1, scale: 1.4, x: 220, y: 100 })
-      .to(elements.current[14], { opacity: 1, scale: 0.5, x: 230, y: -95 })
-      
+    const tl = gsap.timeline();
 
-    
-    tl.to(elements.current, {
-      scale: 10,
-      x: 1000,
-      opacity: 0.2,
-      duration: 10,
-      ease: "power2.in",
-      transformOrigin: "center",
+  
+    tl.to(".char", {      
+      fontSize: 60,      
+      stagger: 1000,   
+      duration: 1000, 
+      lineHeight:'4.5rem'  , 
       scrollTrigger: {
-        trigger: ".open", // The container div
-        start: " top 0%",
-        end: "top 20%",
-        markers:true,
-        toggleActions: "play pause reverse reset", 
-        scrub: 1, // Smooth transition
-        onEnter:()=>console.log("entered"),
-        onLeave:()=>console.log("Left")
-        
-        
-      },
-    });
+        trigger: '.skillContainer',   
+        start: 'top 60%',              
+        end: 'bottom 40%',           
+        scrub: true}              
+    
+    }
+    );
+
+
+  
+    return () => {
+      splitText.revert(); 
+    };
   }, []);
 
   return (
-    <div className="open">
-      {[
-        "develop", "design", "react", "angular", "mySQL", "mongoDB",
-        "typeScript", "restAPI", "Socket.io", "OAuth", "Docker",
-        "AWS", "Mocha", "chai", "node.js",
-      ].map((skill, index) => (
-        <span
-          key={skill}
-          className="skill"
-          ref={(el) => {
-            if (el) elements.current[index] = el;
-          }}
-          id={skill}
-        >
-          {skill}
-        </span>
-      ))}
+    <div >
+      <p className="skillContainer">
+        {skills.join(" ")} 
+   
+  
+      </p>
     </div>
   );
 }
